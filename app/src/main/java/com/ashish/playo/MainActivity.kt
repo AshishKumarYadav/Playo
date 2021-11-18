8f53f5f38485296ea8d1d5e28ea00f559a379b2e
AshishKumarYadav <hiiashish1@gmail.com>
2021-11-18 09:32:12 +0530

    added Internet on/off condition logic

--- app/src/main/java/com/ashish/playo/MainActivity.kt
+++ app/src/main/java/com/ashish/playo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ashish.playo
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,30 +14,59 @@ import com.ashish.playo.api.RetrofitServices
 import com.ashish.playo.repository.MainRepository
 import com.ashish.playo.repository.MainViewModel
 import com.ashish.playo.repository.MyViewModelFactory
+import android.net.NetworkInfo
+
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+
 
 class MainActivity : AppCompatActivity() {
 
 
     lateinit var recyclerView: RecyclerView
+    lateinit var textView: TextView
     var mLayoutManager: LinearLayoutManager?=null
     private val TAG = "MainActivity"
     lateinit var viewModel: MainViewModel
     private val retrofitService = RetrofitServices.getInstance()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initView()
+
+        if (isOnline(this)){
+
+            setAdapter()
+
+        }else{
+
+            recyclerView.visibility=View.GONE
+            textView.visibility=View.VISIBLE
+
+        }
     }
+
+
 
     private fun initView() {
 
         recyclerView = findViewById(R.id.recycler_view)
+        textView=findViewById(R.id.no_internet_text)
 
         mLayoutManager = LinearLayoutManager(this)
         recyclerView.setLayoutManager(mLayoutManager)
+
+
+    }
+
+    private fun setAdapter(){
+
         viewModel = ViewModelProvider(this, MyViewModelFactory(MainRepository(retrofitService))).get(MainViewModel::class.java)
         viewModel.getAllNews()
 
@@ -47,4 +77,21 @@ class MainActivity : AppCompatActivity() {
             adapter.setNewsList(it.articles)
         })
     }
+
+    private fun isOnline(context: Context): Boolean {
+
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+
+        if(activeNetwork?.isConnected!=null){
+            return activeNetwork.isConnected
+        }
+        else{
+            return false
+        }
+    }
+
+
+
 }
